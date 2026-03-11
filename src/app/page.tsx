59fb35be6d46f08455fa91bee1a0e83dd9eba765
mmosbay-hue <mmosbay@gmail.com>
2026-03-11 16:09:52 +0700

    Initial commit by AI Web Builder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client';
-import App from '../App';
+import dynamic from 'next/dynamic';
+
+const App = dynamic(() => import('../App'), { ssr: false });
 
 export default function Page() {
   return <App />;
